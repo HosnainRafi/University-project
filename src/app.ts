bd2a3245db7d9fa5f112153021e0c95e633c07e9
Hosnain Rafi <rhosnain@gmail.com>
2025-07-07 08:00:44 +0600

    Project setup done

--- src/app.ts
+++ src/app.ts
@@ -1,8 +1,10 @@
-import { Request, Response } from "express";
+import express, { Application, Request, Response } from "express";
+import cors from "cors";
 
-const express = require("express");
-const app = express();
-//const port = 3000;
+const app: Application = express();
+
+app.use(express.json());
+app.use(cors());
 
 app.get("/", (req: Request, res: Response) => {
   res.send("Hello World!");
